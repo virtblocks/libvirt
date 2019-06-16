85ebe32031c13ae395d3af7fc4b26665bd27d41e
Andrea Bolognani <abologna@redhat.com>
2019-06-16 18:37:15 +0200

    update virtblockstest

--- tests/virtblockstest.c
+++ tests/virtblockstest.c
@@ -10,6 +10,7 @@ typedef enum {
 } testFlags;
 
 typedef struct {
+    VirtBlocksDevicesMemballoonModel model;
     const char *base;
     const char *ext;
     const char *expect;
@@ -59,6 +60,44 @@ testVirtBlocksUtilBuildFileName(const void *opaque)
     DO_TEST_FILENAME_FULL(_base, _ext, _expect, 0)
 
 static int
+testVirtBlocksDevicesMemballoon(const void *opaque)
+{
+    testInfo *info = (testInfo *) opaque;
+    VIR_AUTOPTR(VirtBlocksDevicesMemballoon) memballoon = NULL;
+    VIR_AUTOFREE(char *) actual = NULL;
+
+    memballoon = virtblocks_devices_memballoon_new();
+
+    virtblocks_devices_memballoon_set_model(memballoon, info->model);
+    actual = virtblocks_devices_memballoon_to_str(memballoon);
+
+    if (!STREQ_NULLABLE(info->expect, actual)) {
+        virTestDifference(stderr, info->expect, actual);
+
+        if (!(info->flags & FLAG_EXPECT_FAILURE))
+            return -1;
+    }
+
+    return 0;
+}
+
+#define DO_TEST_BALLOON_FULL(_model, _expect, _flags) \
+    do { \
+        testInfo info = { \
+            .model = _model, \
+            .expect = _expect, \
+            .flags = _flags, \
+        }; \
+        if (virTestRun("devices::Memballoon", \
+                       testVirtBlocksDevicesMemballoon, &info) < 0) { \
+            ret = -1; \
+        } \
+    } while (0);
+
+#define DO_TEST_BALLOON(_model, _expect) \
+    DO_TEST_BALLOON_FULL(_model, _expect, 0)
+
+static int
 mymain(void)
 {
     int ret = 0;
@@ -66,6 +105,14 @@ mymain(void)
     DO_TEST_FILENAME_ERROR("no-extension", NULL);
     DO_TEST_FILENAME_FAILURE("one", "two", "three");
     DO_TEST_FILENAME("guest", ".xml", "guest.xml");
+
+    DO_TEST_BALLOON(VIRTBLOCKS_DEVICES_MEMBALLOON_MODEL_NONE, "");
+    DO_TEST_BALLOON(VIRTBLOCKS_DEVICES_MEMBALLOON_MODEL_VIRTIO,
+                    "virtio-memballoon");
+    DO_TEST_BALLOON(VIRTBLOCKS_DEVICES_MEMBALLOON_MODEL_VIRTIO_NON_TRANSITIONAL,
+                    "virtio-memballoon-non-transitional");
+    DO_TEST_BALLOON(VIRTBLOCKS_DEVICES_MEMBALLOON_MODEL_VIRTIO_TRANSITIONAL,
+                    "virtio-memballoon-transitional");
 
     return ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
