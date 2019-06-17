f6d19688104fec6e4cf54b913a1da03b074b00de
Andrea Bolognani <abologna@redhat.com>
2019-06-17 13:38:09 +0200

    add libvirt to virtblocks conversion

--- src/rust/rust.h
+++ src/rust/rust.h
@@ -1,5 +1,9 @@
 #pragma once
 
 #include "virtblocks.h"
+#include "conf/domain_conf.h"
 
 VIR_DEFINE_AUTOPTR_FUNC(VirtBlocksDevicesMemballoon, virtblocks_devices_memballoon_free);
+
+int virDomainMemballoonConvertToVirtBlocks(virDomainMemballoonDef *from,
+                                           VirtBlocksDevicesMemballoon **to);
