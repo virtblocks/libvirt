1f2a9f5f7a818631b19f22f4c545dc79957e8039
Andrea Bolognani <abologna@redhat.com>
2019-08-05 17:09:54 +0200

    Temporarily disable memballoon-related code when !Rust
    We don't have a Go implementation accessible from C yet.

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -4124,7 +4124,7 @@ qemuBuildWatchdogCommandLine(virCommandPtr cmd,
 }
 
 
-#ifdef WITH_VIRTBLOCKS
+#ifdef WITH_VIRTBLOCKS_RUST
 static int
 qemuBuildMemballoonCommandLine(virCommandPtr cmd,
                                const virDomainDef *def,
@@ -4149,7 +4149,7 @@ qemuBuildMemballoonCommandLine(virCommandPtr cmd,
 
     return 0;
 }
-#else /* ! WITH_VIRTBLOCKS */
+#else /* ! WITH_VIRTBLOCKS_RUST */
 static int
 qemuBuildMemballoonCommandLine(virCommandPtr cmd ATTRIBUTE_UNUSED,
                                const virDomainDef *def ATTRIBUTE_UNUSED,
@@ -4157,7 +4157,7 @@ qemuBuildMemballoonCommandLine(virCommandPtr cmd ATTRIBUTE_UNUSED,
 {
     return -1;
 }
-#endif /* ! WITH_VIRTBLOCKS */
+#endif /* ! WITH_VIRTBLOCKS_RUST */
 
 
 static char *
