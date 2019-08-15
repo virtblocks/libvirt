f399a5577f95260883e40cb0b8286a826f80b681
Andrea Bolognani <abologna@redhat.com>
2019-08-15 18:13:53 +0200

    ci: Enable Virt Blocks during CI builds
    Use 'check' instead of 'distcheck' because we don't have 'dist'
    working yet; on macOS, we can only build the Rust version since
    we invoke libtool directly when building the Go version and as
    it turns out macOS has its own libtool command which has nothing
    to do with the GNU one we expect.

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -11,3 +11,19 @@
 # Note that this script will have root privileges inside the
 # container, so it can be used for things like installing additional
 # packages.
+
+if which apt-get 2>/dev/null; then
+    # This is currently necessary, at least on Debian 10, to avoid getting
+    # complaints about the 'messagebus' group not existing. Gross, I know
+    dpkg-statoverride --remove /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update
+    apt-get install -y golang cargo rustc
+elif which dnf 2>/dev/null; then
+    dnf update -y --refresh
+    dnf install -y golang cargo rustc
+else
+    echo "Can't install requirements" >&2
+    exit 1
+fi
