2be2b08ca15d1426d8d87c224cd0b1b058f025de
Martin Kletzander <mkletzan@redhat.com>
2019-08-28 10:36:41 +0200

    virtblocks: Unify glue code into one directory and library
    By doing that we also get cleaner and nicer Makefiles.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

---
+++ src/virtblocks_glue/virtblocks_glue.h
@@ -0,0 +1,30 @@
+/*
+ * virtblocks_glue.h: Glue for Virt Blocks code
+ *
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef WITH_VIRTBLOCKS
+# ifdef WITH_VIRTBLOCKS_RUST
+#  include "rust.h"
+# endif /* WITH_VIRTBLOCKS_RUST */
+# ifdef WITH_VIRTBLOCKS_GOLANG
+#  include "golang.h"
+# endif /* WITH_VIRTBLOCKS_GOLANG */
+#endif /* WITH_VIRTBLOCKS */
