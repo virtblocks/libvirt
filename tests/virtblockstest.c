0e0a5b80b3042365a161f85e7379e5de937700c3
Andrea Bolognani <abologna@redhat.com>
2019-09-06 17:28:50 +0200

    virtblocks: Update submodule
    The build_file_name() function has been removed, so we have to drop
    the corresponding test cases.

--- tests/virtblockstest.c
+++ tests/virtblockstest.c
@@ -11,53 +11,9 @@ typedef enum {
 
 typedef struct {
     VirtBlocksDevicesMemballoonModel model;
-    const char *base;
-    const char *ext;
     const char *expect;
     unsigned int flags;
 } testInfo;
-
-static int
-testVirtBlocksUtilBuildFileName(const void *opaque)
-{
-    testInfo *info = (testInfo *) opaque;
-    VIR_AUTOFREE(char *) actual = NULL;
-
-    if (virtblocks_util_build_file_name(&actual, info->base, info->ext) < 0) {
-        if (!(info->flags & FLAG_EXPECT_ERROR))
-            return -1;
-    }
-
-    if (!STREQ_NULLABLE(info->expect, actual)) {
-        virTestDifference(stderr, info->expect, actual);
-
-        if (!(info->flags & FLAG_EXPECT_FAILURE))
-            return -1;
-    }
-
-    return 0;
-}
-
-#define DO_TEST_FILENAME_FULL(_base, _ext, _expect, _flags) \
-    do { \
-        testInfo info = { \
-            .base = _base, \
-            .ext = _ext, \
-            .expect = _expect, \
-            .flags = _flags, \
-        }; \
-        if (virTestRun("util::build_file_name()", \
-                       testVirtBlocksUtilBuildFileName, &info) < 0) { \
-            ret = -1; \
-        } \
-    } while (0);
-
-#define DO_TEST_FILENAME_ERROR(_base, _ext) \
-    DO_TEST_FILENAME_FULL(_base, _ext, NULL, FLAG_EXPECT_ERROR)
-#define DO_TEST_FILENAME_FAILURE(_base, _ext, _expect) \
-    DO_TEST_FILENAME_FULL(_base, _ext, _expect, FLAG_EXPECT_FAILURE)
-#define DO_TEST_FILENAME(_base, _ext, _expect) \
-    DO_TEST_FILENAME_FULL(_base, _ext, _expect, 0)
 
 static int
 testVirtBlocksDevicesMemballoon(const void *opaque)
@@ -101,10 +57,6 @@ static int
 mymain(void)
 {
     int ret = 0;
-
-    DO_TEST_FILENAME_ERROR("no-extension", NULL);
-    DO_TEST_FILENAME_FAILURE("one", "two", "three");
-    DO_TEST_FILENAME("guest", ".xml", "guest.xml");
 
     DO_TEST_BALLOON(VIRTBLOCKS_DEVICES_MEMBALLOON_MODEL_NONE, "");
     DO_TEST_BALLOON(VIRTBLOCKS_DEVICES_MEMBALLOON_MODEL_VIRTIO,
